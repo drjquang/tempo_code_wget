102b4ff28f4ab98091d4fde3fa1dd41b325b59a5
drjquang <38936619+drjquang@users.noreply.github.com>
2024-09-25 06:58:10 +0700

    Create customeTkinter.py
    Create a simple GUI to test customeTkinter library installed successfully or not, and to test Raspberry Pi 4 can build this GUI

---
+++ customeTkinter.py
@@ -0,0 +1,10 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("200x300")
+app.title("Raspberry SQL")
+
+app.mainloop()
